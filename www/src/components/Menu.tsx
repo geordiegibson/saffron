13531b617c020616d19c8d769a29f81c88a847ea
Geordie <ggi28@uclive.ac.nz>
2024-09-14 00:40:19 +1200

    🌞 Fixing Menu on Mobile Light Mode

--- www/src/components/Menu.tsx
+++ www/src/components/Menu.tsx
@@ -6,7 +6,7 @@ const Menu = (props: any) => {
     return (
         <div className="flex flex-col w-screen items-center justify-center">
 
-            <nav className="fixed bottom-4 z-30 mx-auto flex w-max flex-row items-center justify-center gap-1 overflow-visible rounded-full bg-white p-1 shadow-lg ring-1 ring-zinc-300/50 backdrop-blur-sm dark:bg-zinc-900 dark:ring-zinc-700/50">
+            <nav className="fixed bottom-4 z-30 mx-auto flex w-max flex-row items-center justify-center gap-1 overflow-visible rounded-full p-1 shadow-lg ring-1 ring-zinc-300/50 backdrop-blur-sm bg-zinc-900 ring-zinc-700/50">
                                 
                 <Link to="/" className="group relative flex select-none items-center rounded-full bg-white p-3 outline-none transition-all duration-400 hover:bg-slate-200 data-selected:bg-black dark:bg-zinc-900 dark:hover:bg-slate-800 dark:data-selected:bg-white">
                     <svg className="h-5" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
