d23767017ef2b1055e82180a5aa0cc82492f7d30
Geordie <ggi28@uclive.ac.nz>
2024-09-15 00:31:44 +1200

    Temporarily fixing errors for production

--- www/src/pages/Wallet.tsx
+++ www/src/pages/Wallet.tsx
@@ -11,31 +11,31 @@ declare global {
 
 const Wallet = () => {
     
-    const connectKeplr = async () => {
+    // const _ = async () => {
 
-        const sleep = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
+    //     const sleep = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
 
-        while (
-        !window.keplr ||
-        !window.getEnigmaUtils ||
-        !window.getOfflineSignerOnlyAmino
-        ) {
-        await sleep(50);
-        }
+    //     while (
+    //     !window.keplr ||
+    //     !window.getEnigmaUtils ||
+    //     !window.getOfflineSignerOnlyAmino
+    //     ) {
+    //     await sleep(50);
+    //     }
 
-        const CHAIN_ID = "pulsar-3";
-        await window.keplr.enable(CHAIN_ID);
-        const keplrOfflineSigner = window.keplr.getOfflineSignerOnlyAmino(CHAIN_ID);
-        const [{ address: myAddress }] = await keplrOfflineSigner.getAccounts();
+    //     const CHAIN_ID = "pulsar-3";
+    //     await window.keplr.enable(CHAIN_ID);
+    //     const keplrOfflineSigner = window.keplr.getOfflineSignerOnlyAmino(CHAIN_ID);
+    //     const [{ address: myAddress }] = await keplrOfflineSigner.getAccounts();
 
-        new SecretNetworkClient({
-        url: "https://api.pulsar3.scrttestnet.com",
-        chainId: CHAIN_ID,
-        wallet: keplrOfflineSigner,
-        walletAddress: myAddress,
-        encryptionUtils: window.keplr.getEnigmaUtils(CHAIN_ID),
-        });
-    }
+    //     new SecretNetworkClient({
+    //     url: "https://api.pulsar3.scrttestnet.com",
+    //     chainId: CHAIN_ID,
+    //     wallet: keplrOfflineSigner,
+    //     walletAddress: myAddress,
+    //     encryptionUtils: window.keplr.getEnigmaUtils(CHAIN_ID),
+    //     });
+    // }
     
     return (
         <div className="flex flex-col min-h-screen w-screen items-center">
