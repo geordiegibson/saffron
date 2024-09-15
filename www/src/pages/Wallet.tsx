d21b4fad7f1b32ccdf1012a7d37710276adbd8df
Geordie <ggi28@uclive.ac.nz>
2024-09-15 13:05:17 +1200

    💸 Creating Wallet Connection Component

--- www/src/pages/Wallet.tsx
+++ www/src/pages/Wallet.tsx
@@ -1,5 +1,5 @@
 import Menu from "../components/Menu"
-// import { SecretNetworkClient } from "secretjs";
+import { SecretNetworkClient } from "secretjs";
 
 declare global {
     interface Window {
@@ -11,31 +11,31 @@ declare global {
 
 const Wallet = () => {
     
-    // const _ = async () => {
+    async () => {
 
-    //     const sleep = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
+        const sleep = (ms: any) => new Promise((resolve) => setTimeout(resolve, ms));
 
-    //     while (
-    //     !window.keplr ||
-    //     !window.getEnigmaUtils ||
-    //     !window.getOfflineSignerOnlyAmino
-    //     ) {
-    //     await sleep(50);
-    //     }
+        while (
+        !window.keplr ||
+        !window.getEnigmaUtils ||
+        !window.getOfflineSignerOnlyAmino
+        ) {
+        await sleep(50);
+        }
 
-    //     const CHAIN_ID = "pulsar-3";
-    //     await window.keplr.enable(CHAIN_ID);
-    //     const keplrOfflineSigner = window.keplr.getOfflineSignerOnlyAmino(CHAIN_ID);
-    //     const [{ address: myAddress }] = await keplrOfflineSigner.getAccounts();
+        const CHAIN_ID = "pulsar-3";
+        await window.keplr.enable(CHAIN_ID);
+        const keplrOfflineSigner = window.keplr.getOfflineSignerOnlyAmino(CHAIN_ID);
+        const [{ address: myAddress }] = await keplrOfflineSigner.getAccounts();
 
-    //     new SecretNetworkClient({
-    //     url: "https://api.pulsar3.scrttestnet.com",
-    //     chainId: CHAIN_ID,
-    //     wallet: keplrOfflineSigner,
-    //     walletAddress: myAddress,
-    //     encryptionUtils: window.keplr.getEnigmaUtils(CHAIN_ID),
-    //     });
-    // }
+        new SecretNetworkClient({
+        url: "https://api.pulsar3.scrttestnet.com",
+        chainId: CHAIN_ID,
+        wallet: keplrOfflineSigner,
+        walletAddress: myAddress,
+        encryptionUtils: window.keplr.getEnigmaUtils(CHAIN_ID),
+        });
+    }
     
     return (
         <div className="flex flex-col min-h-screen w-screen items-center">
@@ -43,9 +43,44 @@ const Wallet = () => {
         {/* Title */}
         <div className="w-[75vw] lg:w-[50vw]">
             <div className="flex flex-col pt-10 pb-5 top-animation">
-                <p className="text-neutral-400 font-bold">Connect Crypto Wallet</p>
+                <p className="text-neutral-400 font-bold">Select Wallet to Connect</p>
                 <p className="text-white inter" style={{"fontSize": "30px"}}>💵 Wallet</p>
             </div>
+        </div>
+
+
+        <div className="flex flex-col gap-4 p-4 bg-zinc-900 w-[75vw] lg:w-[35vw] h-max rounded-lg mt-6 bottom-animation">
+
+            <div className="flex justify-between pl-6 pr-4 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
+                <p className="text-white font-bold text-lg">Keplr</p>
+                <img src="images/keplr.png" className="h-12 w-12"/>
+            </div>
+
+            <div className="flex justify-between px-6 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
+                <p className="text-white font-bold text-lg">MetaMask</p>
+                <img src="images/metamask.webp" className="h-8 w-8"/>
+            </div>
+
+            <div className="flex justify-between px-6 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
+                <p className="text-white font-bold text-lg">Ledger</p>
+                <img src="images/ledger.png" className="bg-white h-9 w-9"/>
+            </div>
+
+            <div className="flex justify-between pl-6  pr-5 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
+                <p className="text-white font-bold text-lg">Starshell</p>
+                <img src="images/starshell.png" className="h-12 w-12"/>
+            </div>
+
+            <div className="flex justify-between px-6 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
+                <p className="text-white font-bold text-lg">Leap</p>
+                <img src="images/leap.png" className="h-8 w-10"/>
+            </div>
+
+            <div className="flex justify-between pl-6 pr-7 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
+                <p className="text-white font-bold text-lg">Fina</p>
+                <img src="https://is1-ssl.mzstatic.com/image/thumb/Purple112/v4/7b/42/5d/7b425d42-f0ad-0716-2d42-87218eda9e26/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png" className="rounded-full h-8 w-8"/>
+            </div>
+
         </div>
 
         <Menu page='wallet'/>
