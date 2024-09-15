38777d3eb0e30c14090a639c76b358ffc4b9c2b9
Geordie <ggi28@uclive.ac.nz>
2024-09-15 15:53:36 +1200

    🎨 Modifying colour pallete

--- www/src/components/CreateContractModel.tsx
+++ www/src/components/CreateContractModel.tsx
@@ -12,13 +12,10 @@ const CreateContractModel = (props: any) => (
       <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
       <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-neutral-900 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
         <Dialog.Title className="text-white m-0 text-[17px] text-lg font-bold">
-          🌍 Create Contract
+          Create Contract
         </Dialog.Title>
-        <Dialog.Description className="text-gray-400 mt-[10px] mb-5 text-[15px] leading-normal">
-          Select your requirements for a trade. We'll handle the rest.
-        </Dialog.Description>
 
-        <div className="mb-[15px] flex flex-col gap-1">
+        <div className="mb-[15px] flex flex-col gap-1 mt-5">
           <label className="text-gray-400 w-[90px] text-left w-full text-xs" htmlFor="name">You Give</label>
           <CryptoInput />
         </div>
