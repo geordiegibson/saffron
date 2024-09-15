3927c8b02d6bbe079097b6338d75f1f54fd4b460
Geordie <ggi28@uclive.ac.nz>
2024-09-15 13:29:15 +1200

    Updating Wallet Font Size

--- www/src/pages/Wallet.tsx
+++ www/src/pages/Wallet.tsx
@@ -52,32 +52,32 @@ const Wallet = () => {
         <div className="flex flex-col gap-4 p-4 bg-zinc-900 w-[75vw] lg:w-[35vw] h-max rounded-lg mt-6 bottom-animation">
 
             <button onClick={connectKeplrWallet} className="flex justify-between pl-6 pr-4 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
-                <p className="text-white font-bold text-lg geist">Keplr</p>
+                <p className="text-white font-bold geist">Keplr</p>
                 <img src="images/keplr.png" className="h-12 w-12"/>
             </button>
 
             <div className="flex justify-between px-6 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
-                <p className="text-white font-bold text-lg geist">MetaMask</p>
+                <p className="text-white font-bold geist">MetaMask</p>
                 <img src="images/metamask.webp" className="h-8 w-8"/>
             </div>
 
             <div className="flex justify-between px-6 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
-                <p className="text-white font-bold text-lg geist">Ledger</p>
+                <p className="text-white font-bold geist">Ledger</p>
                 <img src="images/ledger.png" className="bg-white h-9 w-9"/>
             </div>
 
             <div className="flex justify-between pl-6  pr-5 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
-                <p className="text-white font-bold text-lg geist">Starshell</p>
+                <p className="text-white font-bold geist">Starshell</p>
                 <img src="images/starshell.png" className="h-12 w-12"/>
             </div>
 
             <div className="flex justify-between px-6 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
-                <p className="text-white font-bold text-lg geist">Leap</p>
+                <p className="text-white font-bold geist">Leap</p>
                 <img src="images/leap.png" className="h-8 w-10"/>
             </div>
 
             <div className="flex justify-between pl-6 pr-7 items-center w-full h-16 rounded-lg bg-zinc-800 hover:bg-zinc-700">
-                <p className="text-white font-bold text-lg geist">Fina</p>
+                <p className="text-white font-bold geist">Fina</p>
                 <img src="https://is1-ssl.mzstatic.com/image/thumb/Purple112/v4/7b/42/5d/7b425d42-f0ad-0716-2d42-87218eda9e26/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png" className="rounded-full h-8 w-8"/>
             </div>
 
